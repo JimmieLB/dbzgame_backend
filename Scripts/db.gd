77c9dc9f3d610fe49e1a05b118f28d59f2665a54
JimmieLB <60014163+JimmieLB@users.noreply.github.com>
2024-08-26 21:54:27 -0400

    attacks update

--- Scripts/db.gd
+++ Scripts/db.gd
@@ -4,7 +4,7 @@ var players = {}
 var rooms = {}
 var waiting_room = {}
 
-const blank_room_template = {"map": 0, "players": {}}
+const blank_room_template = {"map": 0, "players": {}, "attacks": {}} 
 
 const PORT = 4820
 const MAX_CLIENTS = 30
@@ -28,7 +28,7 @@ func _ready():
 
 func _net_tick():
 	for room_id in rooms.keys():
-		for player_id in rooms[room_id]:
+		for player_id in rooms[room_id]["players"]:
 			update_all_players.rpc_id(int(player_id), JSON.stringify(rooms[room_id]))
 
 
@@ -44,6 +44,7 @@ func join_room(room_id, peer_id):
 		"velocity": {"x":0, "y":0}
 		}
 	waiting_room.erase(peer_id)
+	players[str(peer_id)] = room_id
 
 @rpc("any_peer", "call_remote", "reliable")
 func send_create_room(name, peer_id):
@@ -62,7 +63,14 @@ func get_rooms(peer_id):
 @rpc("any_peer")
 func update_player(room_id, peer_id, update):
 	rooms[room_id]["players"][peer_id] = update
-	
+
+@rpc("any_peer")
+func new_attack(room_id, attack_id, update):
+	rooms[room_id]["attacks"][str(attack_id)] = JSON.parse_string(update)
+
+@rpc("any_peer")
+func remove_attack(room_id, attack_id):
+	rooms[room_id]["attacks"].erase(attack_id)
 """
 	SENDERS
 """
@@ -80,11 +88,19 @@ func _on_player_connected(peer_id):
 	welcome_message.rpc_id(peer_id, peer_id)
 	waiting_room[peer_id] = true
 	get_rooms.rpc_id(int(peer_id), JSON.stringify(rooms.keys()))
+	players[str(peer_id)] = ""
 
 func _on_player_disconnected(id):
+	var id_room = players[str(id)]
 	players.erase(id)
 	player_disconnected.emit(id)
 	waiting_room.erase(id)
+	print("Player " + str(id) + " Disconnected")
+	if id_room == "":
+		return
+	rooms[id_room]["players"].erase(int(id))
+	if rooms[id_room]["players"].keys().size() <= 0:
+		rooms.erase(id_room)
 
 func _on_connected_fail(peer_id):
 	print("CONNECTION FAILED")
